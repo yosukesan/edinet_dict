6d7e798368dee02312ff4b9b2b0eaa9ec0738b76
Yosuke OTSUKI <y.otsuki30@gmail.com>
2021-08-24 11:40:45 +0900

    fixed: sample.py

--- sample.py
+++ sample.py
@@ -3,7 +3,11 @@ import json
 
 if __name__=="__main__":
 
+    JP = "様式ツリー-標準ラベル（日本語）"
+    EN = "標準ラベル（英語）"
+
     data = open("edinet_tags.json").read()
     data_json = json.loads(data)
-    print(data_json["jppfs_cor"]["Liabilities"]["en"])
-    print(data_json["jppfs_cor"]["Liabilities"]["jp"])
+
+    print(data_json["jppfs_cor"]["Liabilities"][EN])
+    print(data_json["jppfs_cor"]["Liabilities"][JP])
